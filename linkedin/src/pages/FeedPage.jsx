fb1a2270aa026ece56290d1061af118ca1381e95
Michael Redruello <redruellomichael@gmail.com>
2022-03-31 04:22:15 +0200

    SIdebar hard coded

--- linkedin/src/pages/FeedPage.jsx
+++ linkedin/src/pages/FeedPage.jsx
@@ -1,23 +1,24 @@
-import Feed from '../components/Feed'
-import { Container, Row, Col } from 'react-bootstrap'
-import SideBar from '../components/SideBar'
+import { Container, Row, Col } from "react-bootstrap";
+import Feed from "../components/Feed";
+import SideBar from "../components/SideBar";
+import FeedSideBar from "../components/FeedSideBar";
 
 const ProfilePage = () => {
   return (
     <Container>
       <Row>
-        <Col md={2}>
-          <SideBar />
+        <Col md={3}>
+          <FeedSideBar />
         </Col>
         <Col md={6}>
           <Feed />
         </Col>
-        <Col md={4}>
+        <Col md={3}>
           <SideBar />
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
