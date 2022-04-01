71a11f70b7e6d6f7bce5777f45d0c73b9f21fb09
YojiSato65 <yojisato65@gmail.com>
2022-04-01 15:29:57 +0300

    feed page responsive

--- linkedin/src/pages/FeedPage.jsx
+++ linkedin/src/pages/FeedPage.jsx
@@ -1,24 +1,25 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Feed from "../components/Feed";
-import SideBar from "../components/SideBar";
-import FeedSideBar from "../components/FeedSideBar";
+import { Container, Row, Col } from 'react-bootstrap'
+import FeedSideBar from '../components/FeedSideBar'
+import Feed from '../components/Feed'
+import SideBar from '../components/SideBar'
+import '../styles/feedPage.css'
 
-const ProfilePage = () => {
+const FeedPage = () => {
   return (
     <Container>
       <Row>
-        <Col md={3}>
+        <Col md={12} className="both-sidebars" xl={3}>
           <FeedSideBar />
         </Col>
-        <Col md={6}>
+        <Col md={12} lg={8} xl={6}>
           <Feed />
         </Col>
-        <Col md={3}>
+        <Col md={12} className="both-sidebars" xl={3}>
           <SideBar />
         </Col>
       </Row>
     </Container>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default FeedPage
